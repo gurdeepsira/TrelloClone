c6b967e6468e6ef994188b80cdd7e4e2d97ccc9e
Gurdip Sira <Gurdip.Sira@arup.com>
2019-05-22 17:20:25 +0100

    s

---
+++ TrelloClone/Controllers/BoardsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrelloClone.Controllers
+{
+    public class BoardsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
